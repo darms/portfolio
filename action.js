56c01783e71edd4df6bfa6f5e7201d41359fd5ab
DARCY MCCABE <DARMCCA@GMAIL.COM>
2017-01-02 21:30:32 -0800

    new files created and basic constructor function added to js

---
+++ action.js
@@ -0,0 +1,9 @@
+//variable for images that will link with the different projects -var images = [];
+
+function Project (projName, problemDomain, toolsUsed, solution, timeFrame) {
+  this.projName = projName;
+  this.problemDomain = problemDomain;
+  this.toolsUsed = toolsUsed;
+  this.solution = solution;
+  this.timeFrame = timeFrame;
+}
