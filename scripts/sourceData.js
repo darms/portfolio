f2f85ea89fc515eae0f46f0177eae2ca457d7fb5
DARCY MCCABE <DARMCCA@GMAIL.COM>
2017-01-04 08:27:23 -0800

    basic array filler structure added

---
+++ scripts/sourceData.js
@@ -0,0 +1,20 @@
+var portfolioPieces = [
+  {projName: '',
+  problemDomain: '<p> </p>',
+  toolsUsed: '',
+  solution: '',
+  timeFrame: ''},
+
+  {projName: '',
+  problemDomain: '<p> </p>',
+  toolsUsed: '',
+  solution: '',
+  timeFrame: ''},
+
+  {projName: '',
+  problemDomain: '<p> </p>',
+  toolsUsed: '',
+  solution: '',
+  timeFrame: ''}
+
+]
