9e5b403bb5f3df45b0dcef79a5b6d3ac83cfc2c6
DARCY MCCABE <DARMCCA@GMAIL.COM>
2017-01-06 07:18:36 -0800

    Restructuring HTML and jquery

--- scripts/action.js
+++ scripts/action.js
@@ -15,7 +15,6 @@ var portfolioPieces = [];
 function Project (info) {
   this.projName = info.projName;
   this.problemDomain = info.problemDomain;
-  this.toolsUsed = info.toolsUsed;
   this.solution = info.solution;
   this.timeFrame = info.timeFrame;
 }
@@ -23,3 +22,35 @@ function Project (info) {
 Project.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
+
+//get the different dom elements
+$newArticle.find("#showCase")
+$newArticle.find()
+$newArticle.find()
+$newArticle.find()
+$newArticle.find()
+
+rticle.prototype.toHtml = function() {
+  var $newArticle = $('article.template').clone();
+  $newArticle.find('address a').attr('href', this.client);
+  $newArticle.find('a').text(this.author);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('h2').text(this.client);
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+
+  $newArticle.removeClass('template');
+  return $newArticle;
+
+//render logic- taken from lab day2
+blogArticles.sort(function(currentObject, nextObject) {
+  return (new Date(nextObject.publishedOn)) - (new Date(currentObject.publishedOn));
+});
+
+blogArticles.forEach(function(ele) {
+  articles.push(new Article(ele));
+});
+
+articles.forEach(function(article) {
+  $('#articles').append(article.toHtml());
+});
