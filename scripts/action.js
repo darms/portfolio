9ee29ed219d8bc826ae4b46e249f87120d96ed95
DARCY MCCABE <DARMCCA@GMAIL.COM>
2017-01-09 11:53:41 -0800

    corrected array name so that data pushed and it rendered properly

--- scripts/action.js
+++ scripts/action.js
@@ -1,36 +1,25 @@
+var projects = [];
 
-//refactored on day 2
-//structure influenced by lab on day 2
-
-var portfolioPieces = [];
 function Article (info) {
-  this.title = info.title;
   this.problemDomain = info.problemDomain;
   this.body = info.body;
+  this.title = info.title;
   this.timeFrame = info.timeFrame;
 }
-
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
-  $newArticle.attr('data-category', this.category);
+  $newArticle.find('.problem-body').html(this.problemDomain);
+  $newArticle.find('.solution-body').html(this.body);
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('.timeFrame').html(this.timeFrame);
 
-//get the different dom elements
-$newArticle.find('h1').text(this.title);
-$newArticle.find('.problem-body').html(this.problemDomain);
-$newArticle.find('.solution-body').html(this.body);
-$newArticle.find('.timeFrame').html(this.timeFrame);
   $newArticle.removeClass('template');
   return $newArticle;
+};
 
-//render logic- taken from lab day2
-blogArticles.sort(function(currentObject, nextObject) {
-  return (new Date(nextObject.publishedOn)) - (new Date(currentObject.publishedOn));
+projectArticles.forEach(function(ele) {
+  projects.push(new Article(ele));
 });
-
-blogArticles.forEach(function(ele) {
-  articles.push(new Article(ele));
-});
-
-articles.forEach(function(article) {
-  $('#articles').append(article.toHtml());
+projects.forEach(function(article) {
+  $('#projects').append(article.toHtml());
 });
--- scripts/sourceData.js
+++ scripts/sourceData.js
@@ -1,17 +1,27 @@
-var portfolioPieces = [
-  {projName: 'BusMall',
-    problemDomain: '<p>Aesthetic synth poutine, sustainable humblebrag drinking vinegar poke migas meh bespoke ethical. VHS coloring book celiac, helvetica copper mug vinyl kitsch pop-up subway tile distillery schlitz art party umami selvage tumblr. </p>',
-    solution: '<p>  Snackwave health goth YOLO readymade plaid shabby chic. Intelligentsia selvage pitchfork, pabst four dollar toast shoreditch single-origin coffee bushwick hexagon wayfarers. Chicharrones tilde craft beer messenger bag occupy kinfolk. Bespoke tote bag craft beer mixtape church-key occupy. Normcore meditation YOLO vegan.</p>',
-    timeFrame: '1 week'},
+'use strict';
 
-  {projName: 'Cafe Noir',
-    problemDomain: '<p> Snackwave health goth YOLO readymade plaid shabby chic. Intelligentsia selvage pitchfork, pabst four dollar toast shoreditch single-origin coffee bushwick hexagon wayfarers. Chicharrones tilde craft beer messenger bag occupy kinfolk. Bespoke tote bag craft beer mixtape church-key occupy. Normcore meditation YOLO vegan. </p>',
-    solution: '<p> Snackwave health goth YOLO readymade plaid shabby chic. Intelligentsia selvage pitchfork, pabst four dollar toast shoreditch single-origin coffee bushwick hexagon wayfarers. Chicharrones tilde craft beer messenger bag occupy kinfolk. Bespoke tote bag craft beer mixtape church-key occupy. Normcore meditation YOLO vegan. </p>',
-    timeFrame: '1 week'},
+var projectArticles = [
+  {
 
-  {projName: 'Cookie Stand',
-    problemDomain: '<p> Snackwave health goth YOLO readymade plaid shabby chic. Intelligentsia selvage pitchfork, pabst four dollar toast shoreditch single-origin coffee bushwick hexagon wayfarers. Chicharrones tilde craft beer messenger bag occupy kinfolk. Bespoke tote bag craft beer mixtape church-key occupy. Normcore meditation YOLO vegan. </p>',
-    solution: '<p>  Snackwave health goth YOLO readymade plaid shabby chic. Intelligentsia selvage pitchfork, pabst four dollar toast shoreditch single-origin coffee bushwick hexagon wayfarers. Chicharrones tilde craft beer messenger bag occupy kinfolk. Bespoke tote bag craft beer mixtape church-key occupy. Normcore meditation YOLO vegan.</p>',
-    timeFrame: '2.5 weeks'}
+    title: 'Cafe Noir',
+    problemDomain: '<p><strong>Challenge: </strong>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his</p>',
 
-]
+    body: '<p><strong>Solution: </strong>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his.</p>',
+    timeFrame: '<p><strong> Duration:</strong>2 weeks</p>'
+  },
+  {
+
+    title: 'BusMall',
+        problemDomain: '<p><strong>Challenge:</strong>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his</p>',
+
+    body: '<p><strong>Solution: </strong>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his.</p>',
+    timeFrame: '<p><strong>Duration:</strong>2 weeks</p>'
+  },
+  {
+
+    title: 'Cookie Stand',
+        problemDomain: '<p><strong>Challenge: </strong>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his</p>',
+    body: '<p><strong>Solution: </strong>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his.</p>',
+    timeFrame: '<p><strong>Duration: </strong>2 weeks</p>'
+  },
+];
