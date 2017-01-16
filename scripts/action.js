3b1295b642f0db0037dc68ccba28b8f703f3ea4e
DARCY MCCABE <DARMCCA@GMAIL.COM>
2017-01-15 18:45:34 -0800

    Added more structure to handlebars

--- scripts/action.js
+++ scripts/action.js
@@ -1,21 +1,41 @@
 var projects = [];
-
-function Article (info) {
-  this.problemDomain = info.problemDomain;
-  this.body = info.body;
-  this.title = info.title;
-  this.timeFrame = info.timeFrame;
-}
-Article.prototype.toHtml = function() {
-  var $newArticle = $('article.template').clone();
-  $newArticle.find('.problem-body').html(this.problemDomain);
-  $newArticle.find('.solution-body').html(this.body);
-  $newArticle.find('h2').text(this.title);
-  $newArticle.find('.timeFrame').html(this.timeFrame);
-
-  $newArticle.removeClass('template');
-  return $newArticle;
+function newProj (opts) {
+  for (key in opts) {
+    this[key] = opts[key];
+  }
 };
+
+// function Article (info) {
+//   this.problemDomain = info.problemDomain;
+//   this.body = info.body;
+//   this.title = info.title;
+//   this.timeFrame = info.timeFrame;
+// }
+Article.prototype.toHtml = function() {
+  var source = $('#work').html();
+  var context = projectArticles;
+  var templateRender = Handlebars.compile(source);
+  return templateRender();
+};
+
+projectArticles.forEach(function(fish){
+  projects.push(new Article(fish));
+});
+
+projects.forEach(function(ourNewProjectObject){
+  $('#articles').append(ourNewProjectObject).toHtml();
+});
+
+// Article.prototype.toHtml = function() {
+//   var $newArticle = $('article.template').clone();
+//   $newArticle.find('.problem-body').html(this.problemDomain);
+//   $newArticle.find('.solution-body').html(this.body);
+//   $newArticle.find('h2').text(this.title);
+//   $newArticle.find('.timeFrame').html(this.timeFrame);
+//
+//   $newArticle.removeClass('template');
+//   return $newArticle;
+// };
 
 projectArticles.forEach(function(ele) {
   projects.push(new Article(ele));
